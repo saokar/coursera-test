014648fbba2b8271bcd560c7bde78b329bf81fdb
Vinayak Saokar <vinayak@getinsured.com>
2016-12-06 22:28:55 -0800

    assignment

--- module4-solution/src/data/menudata.service.js
+++ module4-solution/src/data/menudata.service.js
@@ -3,7 +3,7 @@
 
 angular.module('Data')
 .service('MenuDataService', MenuDataService)
-.constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
 
 MenuDataService.$inject = ['$http', 'ApiBasePath']
 function MenuDataService($http, ApiBasePath) {
