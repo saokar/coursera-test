7c85dd1e96c4994cdbf5530d2d837fc7a4806872
Vinayak Saokar <vinayak@getinsured.com>
2016-12-14 15:29:31 -0800

    week 5 assignment

--- module5-solution/src/public/user/signup.controller.js
+++ module5-solution/src/public/user/signup.controller.js
@@ -11,17 +11,7 @@ function SignupController(PrefService,MenuService) {
   reg.basePath = 'https://infinite-savannah-29846.herokuapp.com';
 
   reg.submit = function () {
-    console.log("inside submit");
     this.pref.menuItem = MenuService.getMenuItem(this.pref.menunumber);
-    console.log(this);
-    if(this.pref.menuItem.$$state.value.headers.status == "500") {
-      console.log("short name null");
-    }
-    else {
-       console.log("short name value");
-    }
-    console.log("outside submit");
-
     PrefService.setPref(this.pref);
     reg.completed = true;
   };
